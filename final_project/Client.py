8c9a4ac4c86f2716921509ac3a81a4923a88b185
Oscar Giller <gilleroscar@gmail.com>
2023-05-02 18:11:49 -0400

    added comments and formatting as well as a server side check for success

---
+++ final_project/Client.py
@@ -0,0 +1,31 @@
+import socket
+from pyDH import DiffieHellman
+
+
+p = 2017
+g = 2
+
+
+client_dh_key = DiffieHellman()
+
+# Make the connection to the Server
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 12345))
+print("Connection to Server established")
+
+# Generate and send the public key
+client_public_key = client_dh_key.gen_public_key()
+print("Initial client public key: ", str(client_public_key))
+client_socket.send(client_public_key.to_bytes(2048, 'big'))
+
+# Receive the servers public key and generate the shared secret
+server_public_key_bytes = client_socket.recv(2048)
+server_public_key = int.from_bytes(server_public_key_bytes, 'big')
+
+client_shared_secret = client_dh_key.gen_shared_key(server_public_key)
+
+# Send the shared secret to the server
+client_socket.send(int(client_shared_secret, 16).to_bytes(2048, 'big'))
+client_socket.close()
+
+print("Shared secret key: ", client_shared_secret)
---
+++ final_project/Server.py
@@ -0,0 +1,37 @@
+import socket
+from pyDH import DiffieHellman
+
+p = 2017
+g = 2
+
+server_dh_key = DiffieHellman()
+
+# listen for the connection
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('localhost', 12345))
+server_socket.listen()
+
+# create the connection and send the public key
+client_conn, address = server_socket.accept()
+print("Connection to Client established")
+server_public_key = server_dh_key.gen_public_key()
+print("Initial server public key: ", server_public_key)
+client_conn.send(server_public_key.to_bytes(2048, 'big'))
+
+# receive the client's public key and generate the shared key
+client_public_key_bytes = client_conn.recv(2048)
+client_public_key = int.from_bytes(client_public_key_bytes, 'big')
+print("Received Client public key: ", client_public_key)
+
+server_shared_secret = server_dh_key.gen_shared_key(client_public_key)
+
+# receive the shared key
+client_shared_secret_bytes = client_conn.recv(2048)
+client_shared_secret = hex(int.from_bytes(client_shared_secret_bytes, 'big'))
+client_conn.close()
+print("Server's shared secret key: ", server_shared_secret)
+print("Client's shared secret key: ", str(client_shared_secret)[2:])
+if server_shared_secret == str(client_shared_secret)[2:]:
+    print("SUCCESS: the shared secret keys are identical\n\n")
+else:
+    print("FAILURE: the shared secret keys are not identical\n\n")
